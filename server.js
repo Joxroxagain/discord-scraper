d98334b9292e7efbc05f42bfe84954377ef70dd4
Joe Scott <jox.rox.js@gmail.com>
2021-06-28 14:46:05 -0400

    Add ping pong

--- server.js
+++ server.js
@@ -1,32 +1,39 @@
-var WebSocket = require('ws');
+var WebSocket = require("ws");
 
 class SocketServer {
-    constructor() {
-        this.port = (process.env.PORT || 8080);
-        this.wss = new WebSocket.Server({ port: this.port })
-            .on('listening', () => { console.log('wss on port ' + this.port) })
-            .on('connection', function connection(w) {
-                w.on('message', function (msg) {
-                    console.log('message from client: ' + msg);
-                });
-                w.on('close', function () {
-                    console.log('closing connection');
-                });
-                w.on('error', (e) => { console.log(e) });
-            });
-    }
-    send(data) {
-        this.wss.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(data);
-            }
+  constructor() {
+    this.port = process.env.PORT || 8080;
+    this.wss = new WebSocket.Server({ port: this.port })
+      .on("listening", () => {
+        console.log("wss on port " + this.port);
+      })
+      .on("connection", function connection(w) {
+        w.on("message", function (msg) {
+          data = data.toString("utf8");
+          if (data === "PING") {
+            ws.send("PONG");
+          }
+          console.log("message from client: " + msg);
         });
-    }
+        w.on("close", function () {
+          console.log("closing connection");
+        });
+        w.on("error", (e) => {
+          console.log(e);
+        });
+      });
+  }
+  send(data) {
+    this.wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  }
 }
 
-
 function startServer() {
-    return new SocketServer();
+  return new SocketServer();
 }
 
 module.exports = startServer;
